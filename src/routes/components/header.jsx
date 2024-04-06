c4ed3333325e49aaba53af965f8e3b6758884bf0
Vishesh Gupta <gupta.v.vishesh@gmail.com>
2024-04-05 19:11:04 -0500

    made header look pretty

--- src/routes/components/header.jsx
+++ src/routes/components/header.jsx
@@ -9,18 +9,17 @@ export default function Header() {
 			</div>
 			<div className={styles.open}>
 				<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M0 1L16 1" stroke="currentColor" strokeWidth="2"></path>
-					<path d="M0 6L16 6" stroke="currentColor" strokeWidth="2"></path>
-					<path d="M0 11L16 11" stroke="currentColor" strokeWidth="2"></path>
+					<path d="M0 1L16 1" stroke="currentColor" strokeWidth="2" />
+					<path d="M0 6L16 6" stroke="currentColor" strokeWidth="2" />
+					<path d="M0 11L16 11" stroke="currentColor" strokeWidth="2" />
 				</svg>
 			</div>
 			<div className={styles.close}>
 				<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M0.757812 9.24268L9.24309 0.757393" stroke="currentColor" strokeWidth="2"></path>
-					<path d="M0.757812 0.757324L9.24309 9.24261" stroke="currentColor" strokeWidth="2"></path>
+					<path d="M0.757812 9.24268L9.24309 0.757393" stroke="currentColor" strokeWidth="2" />
+					<path d="M0.757812 0.757324L9.24309 9.24261" stroke="currentColor" strokeWidth="2" />
 				</svg>
 			</div>
-
 		</div>
 	)
 }
