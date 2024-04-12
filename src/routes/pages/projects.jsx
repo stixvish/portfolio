78a88923d67ca755417a53b707af8b8581c23e74
Vishesh Gupta <gupta.v.vishesh@gmail.com>
2024-04-12 18:52:14 -0500

    fixed up the resume and projects pages

--- src/routes/pages/projects.jsx
+++ src/routes/pages/projects.jsx
@@ -1,81 +1,73 @@
-import styles from '../styles/projects.module.scss'
+import styles from '../styles/projects.module.scss';
 
-export default function Projects() {
+export default function Resume() {
 	return (
-		<>
-			<div className={styles.main}>
-				<div className={styles.header}>
-					<h1>projects</h1>
-				</div>
-				<div className={styles.content}>
-					<div className={styles.category}>
+		<div className={styles.main}>
+			<div className={styles.header}>
+				<h1>resume</h1>
+			</div>
+			<div className={styles.content}>
+				<div className={styles.section}>
+					<div className={styles.place}>
+						<a href='https://github.com/stixvish/portfolio'>
+							<img src="src/assets/icons/favicon.png" alt="website favicon" />
+						</a>
 						<div className={styles.info}>
-							<a href="https://github.com/stixvish/portfolio-react">
-								<img src="src/assets/icons/favicon.png" className={styles.image} alt="Website Favicon" />
-							</a>
-							<div>
-								<a href="https://github.com/stixvish/portfolio-react">
-									<h3>Personal Portfolio</h3>
-								</a>
-								<p>Personal Project</p>
-								<div className={styles.frameworks}>
-									<p>MongoDB</p>
-									<p>Express</p>
-									<p>React.js</p>
-									<p>Node.js</p>
-								</div>
+							<h3>Personal Portfolio</h3>
+							<p>Personal Project</p>
+							<div className={styles.frameworks}>
+								<p>MongoDB</p>
+								<p>Express.js</p>
+								<p>React.js</p>
+								<p>Node.js</p>
 							</div>
-						</div>
-						<div className={styles.description}>
-							<p>
-								I used a JavaScript framework to make a website that displayed information about me.
-							</p>
-						</div>
-						<div className={styles.info}>
-							<a href="https://github.com/rtwoo/project-spyn">
-								<img src="src/assets/icons/ev3.jpg" className={styles.image} alt="Website Favicon" />
-							</a>
-							<div>
-								<a href="https://github.com/rtwoo/project-spyn">
-									<h3>Maze Robot</h3>
-								</a>
-								<p>Course Project</p>
-								<div className={styles.frameworks}>
-									<p>MATLAB</p>
-									<p>LEGO Mindstorms</p>
-								</div>
-							</div>
-						</div>
-						<div className={styles.description}>
-							<p>
-								I used MATLAB and LEGO Mindstorms to create a robot that traversed a maze autonomously,
-								picking up and dropping off a person at designated spots along the way.
-							</p>
-						</div>
-						<div className={styles.info}>
-							<a href="https://github.com/stixvish/discordBot">
-								<img src="src/assets/icons/discord.png" className={styles.image} alt="Website Favicon" />
-							</a>
-							<div>
-								<a href="https://github.com/stixvish/discordBot">
-									<h3>Discord Bot</h3>
-								</a>
-								<p>Personal Project</p>
-								<div className={styles.frameworks}>
-									<p>Python</p>
-									<p>Discord.py</p>
-								</div>
-							</div>
-						</div>
-						<div className={styles.description}>
-							<p>
-								I made a bot for the platform Discord that performed
-								simple server maintainance and poked fun at my friends.
-							</p>
 						</div>
 					</div>
+					<div className={styles.description}>
+						<p>
+							I used a JavaScript framework to make a website that displayed information about me.
+						</p>
+					</div>
+					<div className={styles.place}>
+						<a href="https://github.com/rtwoo/project-spyn">
+							<img src="src/assets/icons/ev3.jpg" alt="lego mindstorm picture" />
+						</a>
+						<div className={styles.info}>
+							<h3>Maze Robot</h3>
+							<p>Course Project</p>
+							<div className={styles.frameworks}>
+								<p>MATLAB</p>
+								<p>LEGO Mindstorms</p>
+							</div>
+						</div>
+					</div>
+					<div className={styles.description}>
+						<p>
+							I used MATLAB and LEGO Mindstorms to create a robot that traversed a maze autonomously,
+							picking up and dropping off a person at designated spots along the way.
+						</p>
+					</div>
+					<div className={styles.place}>
+						<a href="https://github.com/stixvish/discordBot">
+							<img src="src/assets/icons/discord.png" alt="discord logo" />
+						</a>
+						<div className={styles.info}>
+							<h3>Discord Bot</h3>
+							<p>Personal Project</p>
+							<div className={styles.frameworks}>
+								<p>Discord.py</p>
+								<p>Python</p>
+							</div>
+						</div>
+					</div>
+					<div className={styles.description}>
+						<p>
+							I made a bot for the platform Discord that performed
+							simple server maintainance and poked fun at my friends.
+						</p>
+					</div>
 				</div>
-			</div>	
-		</>
+			</div>
+		</div>
 	)
 }
--- src/routes/pages/resume.jsx
+++ src/routes/pages/resume.jsx
@@ -5,19 +5,13 @@ export default function Resume() {
 		<div className={styles.main}>
 			<div className={styles.header}>
 				<h1>resume</h1>
-				<a href="src\assets\resume\vishResume_S.pdf" download>
-					<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M12 3a1 1 0 0 1 1 1v9.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 1 1 1.414-1.414L11 13.586V4a1 1 0 0 1 1-1Z" fill="#FFFFFF"/>
-						<path d="M6 17a1 1 0 1 0-2 0v.6C4 19.482 5.518 21 7.4 21h9.2c1.882 0 3.4-1.518 3.4-3.4V17a1 1 0 1 0-2 0v.6c0 .778-.622 1.4-1.4 1.4H7.4c-.778 0-1.4-.622-1.4-1.4V17Z" fill="#FFFFFF"/>
-					</svg>
-				</a>
 			</div>
 			<div className={styles.content}>
-				<div className={styles.category}>
-					<h2 className={styles.title}>Education</h2>
-					<div className={styles.info}>
-						<img src="https://pbs.twimg.com/profile_images/920025455079710720/u2eU7QCT_400x400.jpg" className={styles.image} alt="COD Logo" />
-						<div>
+				<div className={styles.section}>
+					<h2>education</h2>
+					<div className={styles.place}>
+						<img src="https://pbs.twimg.com/profile_images/920025455079710720/u2eU7QCT_400x400.jpg" alt="COD Logo" />
+						<div className={styles.info}>
 							<h3>College of DuPage</h3>
 							<p>Associate of Science (A.S.)</p>
 							<p>May 2023 - May 2024 (expected)</p>
@@ -27,13 +21,14 @@ export default function Resume() {
 					<div className={styles.description}>
 						<p>
 							Relevant Coursework:
+							C++ Language Programming,
 							Calculus III,
-							Differential Equations
+							Discrete Mathematics
 						</p>
 					</div>
-					<div className={styles.info}>
+					<div className={styles.place}>
 						<img src="https://pbs.twimg.com/profile_images/1276630009793245184/_5QTqo7X_400x400.jpg" className={styles.image} alt="ASU Crest" />
-						<div>
+						<div className={styles.info}>
 							<h3>Arizona State University</h3>
 							<p>Ira A. Fulton Schools of Engineering</p>
 							<p>August 2021 - December 2022</p>
@@ -50,9 +45,9 @@ export default function Resume() {
 							Digital Design
 						</p>
 					</div>
-					<div className={styles.info}>
+					<div className={styles.place}>
 						<img src="https://image.maxpreps.io/school-mascot/f/1/1/f11e80d5-6129-4ca3-a64b-494007c79230.gif?version=637672846800000000&width=1024&height=1024" className={styles.image} alt="Neuqua Logo" />
-						<div>
+						<div className={styles.info}>
 							<h3>Neuqua Valley High School</h3>
 							<p>High School Diploma</p>
 							<p>August 2017 - June 2021</p>
@@ -68,15 +63,15 @@ export default function Resume() {
 						</p>
 					</div>
 				</div>
-				<div className={styles.category}>
-					<h2 className={styles.title}>Experience</h2>
-					<div className={styles.info}>
+				<div className={styles.section}>
+					<h2>professional experience</h2>
+					<div className={styles.place}>
 						<img src="https://pbs.twimg.com/profile_images/1476237803763773447/bL1_CQLe_400x400.jpg" className={styles.image} alt="NSF Image" />
-						<div>
-							<h3>NSF-Funded 311 Lab</h3>
-							<p>National Science Foundation</p>
-							<p>June 2022 - August 2022</p>
-							<p>Tempe, AZ</p>
+						<div className={styles.info}>
+							<h3>College of DuPage</h3>
+							<p>Associate of Science (A.S.)</p>
+							<p>May 2023 - May 2024 (expected)</p>
+							<p>Glen Ellyn, IL</p>
 						</div>
 					</div>
 					<div className={styles.description}>
@@ -84,13 +79,13 @@ export default function Resume() {
 							I contributed to a data analysis of differing communities in the Boston area to understand the disparity between communities, and engineered an application to display findings using tools such as MongoDB and React.js.
 						</p>
 					</div>
-					<div className={styles.info}>
+					<div className={styles.place}>
 						<img src="https://pbs.twimg.com/profile_images/1011304005710987267/emBfUZKK_400x400.jpg" className={styles.image} alt="Isos Image" />
-						<div>
+						<div className={styles.info}>
 							<h3>Development Intern</h3>
-							<p>Isos Technology</p>
-							<p>June 2022 - August 2022</p>
-							<p>Tempe, AZ</p>
+							<p>Associate of Science (A.S.)</p>
+							<p>May 2023 - May 2024 (expected)</p>
+							<p>Glen Ellyn, IL</p>
 						</div>
 					</div>
 					<div className={styles.description}>
