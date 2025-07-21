b7f505e6f973bc4511ee8fafcf888cb4f25831a6
Vishesh Gupta <gupta.v.vishesh@gmail.com>
2025-07-21 01:27:18 -0500

    adjusted file names and folder structure

---
+++ src/root.jsx
@@ -0,0 +1,14 @@
+import styles from './root.module.scss'
+
+function Root() {
+
+  return (
+    <>
+      <div>
+        <p className={styles.hello}>Hello world!</p>
+      </div>
+    </>
+  )
+}
+
+export default Root
