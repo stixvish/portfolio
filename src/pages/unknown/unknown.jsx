c7065d5f0aa0bee5cce0ab6f197a144ff053e6d9
Vishesh Gupta <gupta.v.vishesh@gmail.com>
2025-07-21 23:32:31 -0500

    implemented simple error page

--- src/pages/unknown/unknown.jsx
+++ src/pages/unknown/unknown.jsx
@@ -4,6 +4,8 @@ function Unknown() {
 
 	return (
 		<div className={styles.main}>
+			<h1>You've found a page that doesn't exist.</h1>
+			<p>Please navigate back to one that does using the menu.</p>
 		</div>
 	)
 
