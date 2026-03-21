e0205531bede06b799ea48033687216da7695f58
Vishesh Gupta <gupta.v.vishesh@gmail.com>
2026-03-21 11:53:38 -0500

    made intro text mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ export default function Home() {
   return (
     <main className="flex min-h-dvh items-center justify-center">
       <div className="text-center">
-        <h1>hello there!</h1>
-        <h1>i&apos;m vishesh.</h1>
-        <div className="flex items-center justify-center gap-3">
-          <p>software engineer</p>
-          <p>&middot;</p>
-          <p>computer science student</p>
-          <p>&middot;</p>
-          <p>eager to learn</p>
+        <div className="leading-tight">
+          <h1 className="text-hero -translate-x-8">hello there!</h1>
+          <h1 className="text-hero translate-x-8">i&apos;m vishesh.</h1>
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-3 mt-4">
+          <p className="text-body">software engineer</p>
+          <p className="text-body hidden md:block">&middot;</p>
+          <p className="text-body">computer science student</p>
+          <p className="text-body hidden md:block">&middot;</p>
+          <p className="text-body">eager to learn</p>
         </div>
       </div>
     </main>
